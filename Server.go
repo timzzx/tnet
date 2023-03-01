da4d8ada9e6ea00be0a31452930278749b866105
timzzx <zzxfreeman@126.com>
2023-03-01 22:32:27 +0800

    修复ctx.Done阻塞问题

--- Server.go
+++ Server.go
@@ -60,10 +60,12 @@ func (s *Server) Start() {
 		// 心跳
 		go s.heartbeat(agent)
 		// 监听连接关闭
-		select {
-		case <-ctx.Done():
-			s.claer(agent)
-		}
+		go func() {
+			select {
+			case <-ctx.Done():
+				s.claer(agent)
+			}
+		}()
 
 	}
 }
